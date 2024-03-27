835d19151c5c30ccce65be0ddf655d1c41d9df6f
bondalina <142501520+bondalina@users.noreply.github.com>
2024-03-27 19:07:09 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,8 @@
-import MailBox from './MailBox';
-import ProductCard from './ProductCard';
-const productData = [
-  {
-    id: '1_product',
-    title: 'Taco X',
-    price: 4.5,
-    hasDiscount: true,
-    description: 'Fhjhvkjfd kfhf irghi khghg jfhg ug urggljsglj jhgjfhgk.',
-  },
-  {
-    id: '2_product',
-    title: 'Taco Large',
-    price: 6.5,
-    hasDiscount: false,
-    description: 'Fhjhvkjfd kfhf irghi khghg jfhg ug urggljsglj jhgjfhgk.',
-  },
-  {
-    id: '3_product',
-    title: 'Taco Small',
-    price: 3.5,
-    hasDiscount: false,
-    description: 'Fhjhvkjfd kfhf irghi khghg jfhg ug urggljsglj jhgjfhgk.',
-  },
-];
 
 function App() {
   return (
     <div>
-      <MailBox />
-      {productData.map(item => {
-        return (
-          <ProductCard
-            key={item.id}
-            title={item.title}
-            price={item.price}
-            hasDiscount={true}
-            description={item.description}
-          />
-        );
-      })}
+      
     </div>
   );
 }
